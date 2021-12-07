3dc30d1ab700532b8cace0ea13b2c63f7b861055
krishnajhapate <ki54.jhapate@gmail.com>
2021-12-07 09:49:43 +0530

    urls added

--- video/urls.py
+++ video/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 
 # urls
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('vide/<str:room>/<str:created>/', views.index, name='index'),
+]
