47123e47b357b80dbb718f77fb34dd67ee022cf1
krishnajhapate <ki54.jhapate@gmail.com>
2021-12-07 09:13:57 +0530

    app configurations added

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'video',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -68,7 +71,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'main.wsgi.application'
+ASGI_APPLICATION = 'main.asgi.application'
 
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
