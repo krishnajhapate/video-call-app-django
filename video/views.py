7bd48e5220450fe108ee7afd65f1b79f35a85fe9
krishnajhapate <ki54.jhapate@gmail.com>
2021-12-09 13:36:10 +0530

    home page logic created

--- video/views.py
+++ video/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from video.models import Chat
 
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        room = request.POST['room']
+        get_room = Chat.objects.filter(room_name=room)
+        if get_room:
+            c= get_room[0]
+            number = c.allowed_users
+            if int(number) < 2:
+                number =2 
+                return redirect(f"/video/{room}/join/")
+        
+        else:
+            create  = Chat.objects.create(room_name=room,allowed_users=1)
+            if create:
+                return redirect(f"/video/{room}/created/")
+
+
     return render(request, "index.html")
 
 
